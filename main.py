bd970455e0dbcc6a6a575b96c628376fb2caba81
sunjianbo945 <js7995@nyu.edu>
2021-06-23 16:01:22 -0400

    init

---
+++ main.py
@@ -0,0 +1,25 @@
+import requests
+from pyquery import PyQuery as pq
+
+
+def main():
+    url = 'https://finance.yahoo.com/quote/BA?p=BA'
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.101 Safari/537.36",
+        "accept-language": "en-US,en;q=0.9",
+        "accept": "*/*",
+        "accept-encoding": "gzip, deflate, br"
+    }
+
+    response = requests.get(url, headers=headers)
+    html = response.content
+    doc = pq(html)
+    summaries = doc.find("div#quote-summary")
+    rows = summaries.find("tr")
+
+    for row in rows:
+        print('{} = {}'.format(pq(row).find('td').eq(0).text(), pq(row).find('td').eq(1).text()))
+
+
+if __name__ == '__main__':
+    main()
